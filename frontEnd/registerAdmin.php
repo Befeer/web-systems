4d650e355310df525a4c9adb2f1ad5ac12b97563
Befeer <cliffordgauken@gmail.com>
2025-11-23 00:48:09 +0800

    Enhance registration form with labels and error message
    Added labels for form fields and an error message for password mismatch.

--- frontEnd/registerAdmin.php
+++ frontEnd/registerAdmin.php
@@ -10,22 +10,27 @@
     <div>
         <form action="../backEnd/AdminController.php?method_finder=register_admin" method="POST" onsubmit="return validatePassword();">
             <div>
+                <label for="fname">First Name</label>
                 <input type="text" name="fname" placeholder="First Name" required>
             </div>
             <br>
             <div>
+                <label for="lname">First Name</label>
                 <input type="text" name="lname" placeholder="Last Name" required>
             </div>
             <br>
             <div>
+                <label for="Username">Username</label>
                 <input type="text" name="username" placeholder="Username" required>
             </div>
             <br>
             <div>
+                <label for="Email">Email</label>
                 <input type="email" name="email" placeholder="Email" required>
             </div>
             <br>
             <div>
+                <label for="office">Office</label>
                 <select name="office" required>
                     <option value="BYTe">BYTe</option>
                     <option value="LISA">LISA</option>
@@ -36,16 +41,20 @@
             </div>
             <br>
             <div>
+                <label for="is_ultimate_admin">Is Admin</label>
                 <input type="radio" name="is_ultimate_admin" value="1"> Admin
                 <input type="radio" name="is_ultimate_admin" value="0" checked> User          
             </div>
             <br>
             <div>
+                <label for="admin_password">Password</label>
                 <input type="password" name="admin_password" id="admin_password" placeholder="Password" required>
             </div>
             <br>
             <div>
+                <label for="admin_confirm">Confirm password</label>
                 <input type="password" name="admin_confirm" id="admin_confirm" placeholder="Confirm password" required>
+                <p id="error" style="display:none; color:red; font-weight:bold;">Password do not match</p>
             </div>
             <br>
             <div>
